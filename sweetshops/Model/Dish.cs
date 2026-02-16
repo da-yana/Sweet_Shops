07482419f60d830c2e5a4e36087122acb9c35ad7
st403 <st403@WS403-12>
2026-02-16 09:11:28 +0300

    Сделана вторая задача

---
+++ sweetshops/Model/Dish.cs
@@ -0,0 +1,10 @@
+﻿namespace sweetshops.Model
+{
+    public class Dish : EFModel
+    {
+        public string? CategoriesDish  { get; set; }
+        public int Price { get; set; }
+        public int CookingTime { get; set; }
+
+    }
+}
---
+++ sweetshops/Model/EFModel.cs
@@ -0,0 +1,8 @@
+﻿namespace sweetshops.Model
+{
+    public class EFModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
